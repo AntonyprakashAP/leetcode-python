1fcf3793ba87e41dee38c9a4445ddacb9d4d58d0
antonyprakash <antony.prakash@asxtec.com>
2026-03-02 23:33:09 +0530

    feb 2

---
+++ 1-03-26.py
@@ -0,0 +1,18 @@
+from typing import Counter
+
+
+class Solution:
+    def lengthOfLongestSubstring(s: str) -> int:
+        cnt = Counter()
+        ans = l = 0
+        for r, c in enumerate(s):
+            cnt[c] += 1
+            while cnt[c] > 1:
+                cnt[s[l]] -= 1
+                l += 1
+            ans = max(ans, r - l + 1)
+        return ans
+
+
+    print(lengthOfLongestSubstring("abcabcbb"))
+        
